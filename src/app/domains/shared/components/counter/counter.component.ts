b9581603fbb63b46bff4615bdeb7ce8b816b7efe
ltorresu31 <ltorresu21@gmail.com>
2024-04-26 23:08:15 -0500

    Agregados componentes counter y about

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
+
+  constructor() {
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+}
