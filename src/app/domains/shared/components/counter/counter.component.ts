a413c6dbbb98c62074d9eb5ec3c7b4d50d543827
ltorresu31 <ltorresu21@gmail.com>
2024-04-27 09:42:11 -0500

    ciclo de vida counter

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -13,6 +13,21 @@ export class CounterComponent {
 
   constructor() {
     console.log('Constructor ');
+    console.log('-'.repeat(10));
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges ');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+  ngOnInit(){
+    console.log('ngOnInit ');
+    console.log('-'.repeat(10));
+    console.log('duration => ', this.duration)
+    console.log('message => ', this.message)
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
     console.log('-'.repeat(10));
   }
 }
